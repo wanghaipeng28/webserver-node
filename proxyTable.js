e8d2e5488f6b360b4397659a0b2ae45ad538e07d
wanghaipeng28 <wanghaipeng28@163.com>
2018-07-02 14:37:21 +0800

    modified:   proxyTable.js

--- proxyTable.js
+++ proxyTable.js
@@ -2,21 +2,28 @@
  * Created by Nantian on 2018/7/2.
  */
 module.exports = {
-  '/nantian-sdn/nw/integration': {//测试上传代理接口
-    target: 'http://10.12.248.14',
+  '/nantian-sdn/nw/integration': {
+    target: 'http://10.12.248.13:8080',
     changeOrigin: true,
     pathRewrite: {
-      '^/nantian-sdn/nw/file': '/nantian-sdn/nw/integration'
+      '^/nantian-sdn/nw/integration': '/nantian-sdn/nw/integration'
     }
   },
-  '/nantian-sdn/nw/ip': {//测试上传代理接口
-    target: 'http://10.12.248.14',
+  '/nantian-sdn/nw/ip': {
+    target: 'http://10.12.248.13:8080',
     changeOrigin: true,
     pathRewrite: {
-      '^/nantian-sdn/nw/file': '/nantian-sdn/nw/ip'
+      '^/nantian-sdn/nw/ip': '/nantian-sdn/nw/ip'
     }
   },
-  '/nantian-sdn/nw/southservice': {//测试上传代理接口
+  '/nantian-sdn/nw/file': {//测试上传代理接口
+    target: 'http://10.12.248.13:8080',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/nantian-sdn/nw/file': '/nantian-sdn/nw/file'
+    }
+  },
+  '/nantian-sdn/nw/southservice': {
     target: 'http://10.12.248.11:8092',
     changeOrigin: true,
     pathRewrite: {
